71f13922b44aea6c7b8ba5e889d7dd0f0cb62f4b
Kaustubh Mani <kaustubh3095@gmail.com>
2021-02-23 04:48:13 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .monolayout import MonoLayout
-from .monooccupancy import MonoOccupancy
-from .pseudolidar_enet import PseudoLidar_ENet
-from .pseudolidar_unet import PseudoLidar_UNet
-from .videolayout import VideoLayout
